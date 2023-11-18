7391fbe7aba9d434c951c1f244f00584e6830672
SauravKanchan <sauravnk30@gmail.com>
2023-11-18 19:10:35 +0300

    extract ipfs hash but facing issue in fetching in ipfs data becuase of csp

---
+++ extension/config.js
@@ -0,0 +1,103 @@
+export const ADDRESS = "0xdDDE06DbBb5489DbCc2CB47148D491e147e90171";
+export const RPC = "https://goerli.base.org";
+export const ABI = [
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "id",
+        type: "uint256",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "string",
+        name: "ipfs_hash",
+        type: "string",
+      },
+    ],
+    name: "LinkCreated",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_id",
+        type: "uint256",
+      },
+      {
+        internalType: "string",
+        name: "_ipfs_hash",
+        type: "string",
+      },
+    ],
+    name: "changeIpfsHash",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_ipfs_hash",
+        type: "string",
+      },
+    ],
+    name: "createLink",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "_id",
+        type: "uint256",
+      },
+    ],
+    name: "getLink",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "linkCount",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
--- extension/src/index.js
+++ extension/src/index.js
@@ -1,95 +1,31 @@
 const ethers = require("ethers");
 let tweets = new Set();
-import div from "./div.html";
+import { ABI, ADDRESS } from "../config";
+import form from "./components/form.html";
 
-function checkIfReactRendered() {
+async function checkIfReactRendered() {
   const tweet_divs = document.querySelectorAll('div[data-testid="tweetText"]');
-  console.log(div)
   if (tweet_divs.length > 0) {
-    tweet_divs.forEach((tweet_div) => {
+    tweet_divs.forEach(async (tweet_div) => {
       // check tweet_div exists in tweets
       if (!tweets.has(tweet_div)) {
+        tweets.add(tweet_div);
         // check whether tweet_div.innerText has a https://x3.fyi/id/ link
         if (tweet_div.innerText.includes("https://x3.fyi/id/")) {
-          tweet_div.innerHTML =
-            tweet_div.innerHTML +
-            `<div style="width: 100%; display: flex;">
-            <input type="text" style="flex: 1 !important; margin-right: 10px !important;     -webkit-text-size-adjust: 100%;
-    tab-size: 4;
-    box-sizing: border-box;
-    border-style: solid;
-    --tw-border-spacing-x: 0;
-    --tw-border-spacing-y: 0;
-    --tw-translate-x: 0;
-    --tw-translate-y: 0;
-    --tw-rotate: 0;
-    --tw-skew-x: 0;
-    --tw-skew-y: 0;
-    --tw-scale-x: 1;
-    --tw-scale-y: 1;
-    --tw-pan-x: ;
-    --tw-pan-y: ;
-    --tw-pinch-zoom: ;
-    --tw-scroll-snap-strictness: proximity;
-    --tw-gradient-from-position: ;
-    --tw-gradient-via-position: ;
-    --tw-gradient-to-position: ;
-    --tw-ordinal: ;
-    --tw-slashed-zero: ;
-    --tw-numeric-figure: ;
-    --tw-numeric-spacing: ;
-    --tw-numeric-fraction: ;
-    --tw-ring-inset: ;
-    --tw-ring-offset-width: 0px;
-    --tw-ring-offset-color: #fff;
-    --tw-ring-color: rgb(59 130 246 / 0.5);
-    --tw-ring-offset-shadow: 0 0 #0000;
-    --tw-ring-shadow: 0 0 #0000;
-    --tw-shadow: 0 0 #0000;
-    --tw-shadow-colored: 0 0 #0000;
-    --tw-blur: ;
-    --tw-brightness: ;
-    --tw-contrast: ;
-    --tw-grayscale: ;
-    --tw-hue-rotate: ;
-    --tw-invert: ;
-    --tw-saturate: ;
-    --tw-sepia: ;
-    --tw-drop-shadow: ;
-    --tw-backdrop-blur: ;
-    --tw-backdrop-brightness: ;
-    --tw-backdrop-contrast: ;
-    --tw-backdrop-grayscale: ;
-    --tw-backdrop-hue-rotate: ;
-    --tw-backdrop-invert: ;
-    --tw-backdrop-opacity: ;
-    --tw-backdrop-saturate: ;
-    --tw-backdrop-sepia: ;
-    font-family: inherit;
-    font-feature-settings: inherit;
-    font-variation-settings: inherit;
-    font-weight: inherit;
-    margin: 0;
-    display: block;
-    width: 100%;
-    border-radius: 0.5rem;
-    border-width: 1px;
-    padding: 0.5rem;
-    padding-left: 1rem;
-    font-size: 0.875rem;
-    line-height: 1.25rem;
-    --tw-border-opacity: 1;
-    border-color: rgb(75 85 99 / var(--tw-border-opacity));
-    --tw-bg-opacity: 1;
-    background-color: rgba(55 65 81 0));
-    --tw-text-opacity: 1;
-    color: rgb(255 255 255 / var(--tw-text-opacity));">
-            <a href="/i/verified-choose" role="link" class="css-4rbku5 css-18t94o4 css-1dbjc4n r-l5o3uw r-42olwf r-sdzlij r-1phboty r-rs99b7 r-1loqt21 r-2yi16 r-1qi8awa r-1ny4l3l r-ymttw5 r-o7ynqc r-6416eg r-lrvibr"><div dir="ltr" class="css-901oao r-1awozwy r-jwli3a r-6koalj r-18u37iz r-16y2uox r-37j5jr r-a023e6 r-b88u0q r-1777fci r-rjixqe r-bcqeeo r-q4m81j r-qvutc0" style="text-overflow: unset;"><span class="css-901oao css-16my406 css-1hf3ou5 r-poiln3 r-a023e6 r-rjixqe r-bcqeeo r-qvutc0" style="text-overflow: unset;"><span class="css-901oao css-16my406 r-poiln3 r-bcqeeo r-qvutc0" style="text-overflow: unset;">Submit</span></span></div></a>
-          </div>
-          `;
-        }
+          let match = tweet_div.innerText.match(/(x3\.fyi\/id\/(\d+))/);
+          let extractedNumber = match ? match[2] : null;
 
-        tweets.add(tweet_div);
+          await window.ethereum.enable();
+          let provider = new ethers.providers.Web3Provider(window.ethereum);
+          const contract = new ethers.Contract(ADDRESS, ABI, provider);
+          let data = await contract.getLink(extractedNumber);
+          // make a get request to https://gateway.lighthouse.storage/ipfs/
+          let response = await fetch(
+            "https://gateway.lighthouse.storage/ipfs/" + data[1]
+          );
+          console.log(response.data);
+          tweet_div.innerHTML = tweet_div.innerHTML + form;
+        }
       }
     });
   } else {
